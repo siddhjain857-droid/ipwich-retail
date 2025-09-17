51d0edeb48e6ae8dd0791ab3a34acfb5307487ee
siddhjain857-droid <siddhjain857@gmail.com>
2025-09-18 03:50:35 +0530

    rendering

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 # users/urls.py
+from django import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path('signup/', views.signup, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# users/views.py
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)  # Create user but don't save password yet
+            user.set_password(form.cleaned_data['password'])  # Encrypt password
+            user.save()
+            login(request, user)  # Log in the user automatically after registration
+            return redirect('home')  # Redirect to homepage or any other page
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/signup.html', {'form': form})
