f40469cca495d5ab1742a3e653f602cf16bbbf22
siddhjain857-droid <siddhjain857@gmail.com>
2025-09-18 16:15:48 +0530

    committing again after making some changes for re-deployment

--- retail/settings.py
+++ retail/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 DEBUG = os.getenv("DEBUG", "True") == "True"  
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1", "render.com").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1",).split(",")
 
 
 # Application definition
