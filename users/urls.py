f23bc69b7787ad15d5c1d34bd3bd459581a7ef73
siddhjain857-droid <siddhjain857@gmail.com>
2025-09-18 16:22:58 +0530

    committing again after making some changes for re-deployment

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
-    path('signup/', views.signup, name='signup'),
+    # path('signup/', views.signup, name='signup'),
 ]
